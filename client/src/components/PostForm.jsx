e849da75d6aa8432a85819f95f1c3d7dc75b9e0d
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-29 15:08:11 -0500

    added button for submitting and checking if post is valid

--- client/src/components/PostForm.jsx
+++ client/src/components/PostForm.jsx
@@ -17,6 +17,22 @@ const PostForm = () =>
         }, []
     )
 
+    const onSubmit = () =>
+    {
+        if(postIsValid())
+        {
+
+        } else
+        {
+            window.alert("post is invalid")
+        }
+    }
+
+    const postIsValid = () =>
+    {
+        return title != "" && body != "" && categoryId != 0 && imageURL != ""
+    }
+
     return <div>
         <div>
             <label htmlFor="title">Title: </label>
@@ -35,11 +51,14 @@ const PostForm = () =>
         </select>
         <div>
             <label htmlFor="image">Header URL: </label>
-            <input type="text" id="image" value={imageURL} onChange={(e) => setImageURL(e.target.value)}/>
+            <input type="text" id="image" value={imageURL} onChange={(e) => setImageURL(e.target.value)} />
         </div>
         <div>
             <label htmlFor="date">Publish Date: </label>
-            <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)}/>
+            <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} />
+        </div>
+        <div>
+            <button onClick={onSubmit}>Submit</button>
         </div>
     </div>
 }
