40af6cb3c3fb4dfbde1515c6b701508846b0e0f3
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-29 14:42:54 -0500

    added inputs for each field

--- client/src/components/PostForm.jsx
+++ client/src/components/PostForm.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react"
+import { getAllCategories } from "../managers/categories"
 
 const PostForm = () =>
 {
-    return <>post form</>
+    const [categories, setCategories] = useState([])
+
+    useEffect(
+        () =>
+        {
+            getAllCategories().then(setCategories)
+        }, []
+    )
+
+    return <div>
+        <div>
+            <label htmlFor="title">Title: </label>
+            <input type="text" id="title" />
+        </div>
+        <div>
+            <label htmlFor="body">Body: </label>
+            <input type="text" id="body" />
+        </div>
+        <select>
+            <option defaultChecked>Select A Category</option>
+            {
+                categories.map(c =>
+                    <option key={"c" + c.id}>{c.categoryName}</option>)
+            }
+        </select>
+        <div>
+            <label htmlFor="image">Header URL: </label>
+            <input type="text" id="image" />
+        </div>
+        <div>
+            <label htmlFor="date">Header URL: </label>
+            <input type="date" id="date" />
+        </div>
+    </div>
 }
 
 export default PostForm
