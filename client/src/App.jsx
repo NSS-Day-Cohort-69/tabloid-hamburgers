741e3a3a8934955281565a4840ab4371984afd99
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-30 09:49:03 -0500

    added react context for user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { tryGetLoggedInUser } from "./managers/authManager";
 import { Spinner } from "reactstrap";
 import NavBar from "./components/NavBar";
 import ApplicationViews from "./components/ApplicationViews";
+
+export const UserContext = createContext()
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState();
@@ -22,13 +24,13 @@ function App() {
   }
 
   return (
-    <>
+    <UserContext.Provider value={loggedInUser}>
       <NavBar loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
       <ApplicationViews
         loggedInUser={loggedInUser}
         setLoggedInUser={setLoggedInUser}
       />
-    </>
+    </UserContext.Provider>
   );
 }
 
