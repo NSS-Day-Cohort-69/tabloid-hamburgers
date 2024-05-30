0b2600e610f103ffdb009f4055ec0fbaa494b8f3
Lucas Bresson <pandaboy816@gmail.com>
2024-05-30 15:55:46 -0500

    Fixed bugs relating to tags

--- client/src/managers/tags.js
+++ client/src/managers/tags.js
@@ -17,5 +17,13 @@ export const updateTagByTagObject = (tagObject) => {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(tagObject),
-  }).then((res) => res.json());
+  });
+};
+
+export const addATag = (tagObject) => {
+  return fetch(_apiUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(tagObject),
+  });
 };
