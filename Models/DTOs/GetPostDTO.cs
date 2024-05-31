57cb5e35fe9fbcc46536c3ab70e1d44d81cf7e83
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-31 14:15:05 -0500

    updated GetPostDTO and UserProfileForGetPostDTO to have constructor for DRY principle purposes

--- Models/DTOs/GetPostDTO.cs
+++ Models/DTOs/GetPostDTO.cs
@@ -18,5 +18,25 @@ public class GetPostDTO
 
     public DateTime? Publication { get; set; }
     public List<int> TagIds { get; set; }
-    public List<GetPostCommentsDTO> PostComments { get; set; }
+    public List<GetPostCommentsDTO> Comments { get; set; }
+
+    public GetPostDTO(Post post)
+    {
+        if (post.Comments == null || post.Author == null)
+        {
+            throw new Exception("Must include comments and author");
+        }
+
+        Id = post.Id;
+        Title = post.Title;
+        AuthorId = post.AuthorId;
+        Content = post.Content;
+        ImageURL = post.ImageURL;
+        CategoryId = post.CategoryId;
+        IsApproved = post.IsApproved;
+        Publication = post.Publication;
+        TagIds = post.PostTags.Select(pt => pt.TagId).ToList();
+        Comments = post.Comments.Select(c => new GetPostCommentsDTO(c)).ToList();
+        Author = new UserProfileForGetPostDTO(post.Author);
+    }
 }
--- Models/DTOs/UserProfileForGetPostDTO.cs
+++ Models/DTOs/UserProfileForGetPostDTO.cs
@@ -1,14 +1,17 @@
-
-
-
 namespace Tabloid.Models.DTOs;
 
 public class UserProfileForGetPostDTO
 {
     public int Id { get; set; }
 
-    public string UserName { get; set; }  
+    public string UserName { get; set; }
 
-    public string FirstName {get; set;}
-   
+    public string FirstName { get; set; }
+
+    public UserProfileForGetPostDTO(UserProfile userProfile)
+    {
+        Id = userProfile.Id;
+        FirstName = userProfile.FirstName;
+        UserName = userProfile.UserName;
+    }
 }
