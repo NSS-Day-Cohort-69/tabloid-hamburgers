7c1131bf2edf5595502f644a39d77b61e8c30d9d
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-31 13:45:02 -0500

    shows if user is admin and sorted alphabetically

--- client/src/components/userprofiles/UserProfilesList.jsx
+++ client/src/components/userprofiles/UserProfilesList.jsx
@@ -2,13 +2,33 @@ import { useEffect, useState } from "react";
 import { getProfiles } from "../../managers/userProfileManager";
 import { Link } from "react-router-dom";
 
-export default function UserProfileList() {
+export default function UserProfileList()
+{
   const [userprofiles, setUserProfiles] = useState([]);
 
-  const getUserProfiles = () => {
-    getProfiles().then(setUserProfiles);
+  const getUserProfiles = () =>
+  {
+    getProfiles().then(
+      (gottenProfiles) =>
+      {
+        gottenProfiles = gottenProfiles.sort(function (a, b)
+        {
+          if(a.userName < b.userName)
+          {
+            return -1;
+          }
+          if(a.userName > b.userName)
+          {
+            return 1;
+          }
+          return 0;
+        });
+        setUserProfiles(gottenProfiles)
+      }
+    );
   };
-  useEffect(() => {
+  useEffect(() =>
+  {
     getUserProfiles();
   }, []);
   return (
@@ -16,7 +36,7 @@ export default function UserProfileList() {
       <p>User Profile List</p>
       {userprofiles.map((p) => (
         <p key={p.id}>
-          {p.firstName} {p.lastName} {p.userName}{" "}
+          {p.firstName} {p.lastName} {p.userName} {p.roles.includes("Admin") ? "admin" : "author"}
           <Link to={`/userprofiles/${p.id}`}>Details</Link>
         </p>
       ))}
