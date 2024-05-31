233bd98b24c00b97341b9b7fa24bacf53e97a598
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-31 09:06:26 -0500

    added delete button

--- client/src/components/PostDetails.jsx
+++ client/src/components/PostDetails.jsx
@@ -1,19 +1,22 @@
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { getPostById } from "../managers/postManager";
+import { UserContext } from "../App";
 
 
-
-export default function PostDetails() {
+export default function PostDetails()
+{
     const [post, setPost] = useState({});
     const { postId } = useParams();
+    const user = useContext(UserContext)
 
-    const getAndResetPost = () => {
+    const getAndResetPost = () =>
+    {
         getPostById(postId).then(setPost);
     };
 
-    useEffect(() => {
+    useEffect(() =>
+    {
         getAndResetPost();
     }, []);
 
@@ -25,11 +28,14 @@ export default function PostDetails() {
                 key={post.id}
                 className=""
             >
-               <img>{post.imageURL}</img>
-                <p>{post.content}</p> 
+                <img>{post.imageURL}</img>
+                <p>{post.content}</p>
                 <p>{post.publicationDate}</p>
                 <p>{post.author?.firstName}</p>
-
+                {
+                    user.id == post.authorId
+                    && <button>Delete</button>
+                }
             </div>
 
         </>
