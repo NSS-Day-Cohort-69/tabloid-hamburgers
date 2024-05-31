9c22e79ce5681616f987af918c7ef57405d40252
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-31 09:18:45 -0500

    delete button deletes post

--- client/src/components/PostDetails.jsx
+++ client/src/components/PostDetails.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
-import { getPostById } from "../managers/postManager";
+import { useNavigate, useParams } from "react-router-dom";
+import { deletePost, getPostById } from "../managers/postManager";
 import { UserContext } from "../App";
 
 
@@ -9,6 +9,8 @@ export default function PostDetails()
     const [post, setPost] = useState({});
     const { postId } = useParams();
     const user = useContext(UserContext)
+
+    const navigate = useNavigate()
 
     const getAndResetPost = () =>
     {
@@ -24,7 +26,7 @@ export default function PostDetails()
     {
         if(window.confirm(`Confirm delete for: ${post.title}`))
         {
-            debugger
+            deletePost(postId).then(() => navigate("/post"))
         }
     }
 
