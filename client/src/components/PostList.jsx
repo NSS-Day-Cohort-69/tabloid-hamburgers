621fe0916f2b88b24f6c8e6b6ae095dd7ce47f3f
Lucas Bresson <pandaboy816@gmail.com>
2024-05-31 14:28:46 -0500

    Posts are filterable by Category

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -3,10 +3,18 @@ import { getPublicPosts } from "../managers/postManager";
 import { Link } from "react-router-dom";
 import { UserContext } from "../App";
 import "./PostView.css";
+import { getAllCategories } from "../managers/categories";
 
 const PostList = () => {
   const user = useContext(UserContext);
+  const [selectedCategory, setSelectedCategory] = useState(0);
   const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getAllCategories().then(setCategories);
+  }, []);
 
   useEffect(() => {
     getPublicPosts().then((posts) => {
@@ -16,11 +24,34 @@ const PostList = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (selectedCategory === 0) {
+      setFilteredPosts(posts);
+    } else {
+      setFilteredPosts(posts.filter((p) => p.categoryId == selectedCategory));
+    }
+  }, [selectedCategory, posts]);
+
   return (
     <main>
       <div className="PostList-div-maincontainer">
+        <select
+          defaultValue={0}
+          onChange={(e) => {
+            setSelectedCategory(parseInt(e.target.value));
+          }}
+        >
+          <option value={0}>--All Categories--</option>
+          {categories.map((c) => {
+            return (
+              <option key={c.id} value={c.id}>
+                {c.categoryName}
+              </option>
+            );
+          })}
+        </select>
         <section className="PostList-section-posts">
-          {posts.map((p) => (
+          {filteredPosts.map((p) => (
             <article key={"p" + p.id} className="PostList-post-border">
               <header className="PostList-post-header">
                 {p.publication}{" "}
